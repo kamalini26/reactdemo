8718677aa4ea5cfb53d16ff5a0dba36f28ef84b9
kamalini26 <43268168+kamalini26@users.noreply.github.com>
2021-07-16 18:29:22 +0530

    Update App.js

--- App.js
+++ App.js
@@ -27,26 +27,32 @@ const App = () => {
         {
             setWinMessage(ticktactoe[6]+"is the winner")     
         }
-        else if((ticktactoe[0]==ticktactoe[3] || ticktactoe[0]==ticktactoe[6])&&(ticktactoe[0]!="")){
+        else if((ticktactoe[0]==ticktactoe[3] || ticktactoe[0]==ticktactoe[6])&&(ticktactoe[0]!=""))
+        {
 
         setWinMessage(ticktactoe[0]+"is the winner")  
     }
-        else if((ticktactoe[1]==ticktactoe[4] || ticktactoe[1]==ticktactoe[7])&&(ticktactoe[1]!="")){
+        else if((ticktactoe[1]==ticktactoe[4] || ticktactoe[1]==ticktactoe[7])&&(ticktactoe[1]!=""))
+        {
         setWinMessage(ticktactoe[1]+"is the winner")  
     }
-    else if((ticktactoe[2]==ticktactoe[5] || ticktactoe[2]==ticktactoe[8])&&(ticktactoe[2]!="")){
+    else if((ticktactoe[2]==ticktactoe[5] || ticktactoe[2]==ticktactoe[8])&&(ticktactoe[2]!=""))
+    {
         setWinMessage(ticktactoe[2]+"is the winner")  
     }
-    else if((ticktactoe[2]==ticktactoe[4] || ticktactoe[2]==ticktactoe[6])&&(ticktactoe[2]!="")){
+    else if((ticktactoe[2]==ticktactoe[4] || ticktactoe[2]==ticktactoe[6])&&(ticktactoe[2]!=""))
+    {
         setWinMessage(ticktactoe[2]+"is the winner")  
     }
-    else if((ticktactoe[0]==ticktactoe[4] || ticktactoe[0]==ticktactoe[8])&&(ticktactoe[0]!="")){
+    else if((ticktactoe[0]==ticktactoe[4] || ticktactoe[0]==ticktactoe[8])&&(ticktactoe[0]!=""))
+    {
         setWinMessage(ticktactoe[0]+"is the winner")  
     }
+}
     return (
         <div>
             <Icon choice="circle" />
         </div>
     )
 }
-export default App;
+export default App
